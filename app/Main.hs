111263160e81db24f9e238e45876bef64dea2c97
radu <radu.buciuceanu@gmail.com>
2021-11-22 18:59:52 +0200

    [master] Use qualified imports in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,38 @@
 module Main where
 
 import Control.Monad.Reader
-import Data.CreateName as CreateNameRepository
-import Data.GetNames as GetNamesRepository
-import Data.UpdateName as UpdateNameRepository
-import Domain.GetNames as GetNamesCommand
-import Ioc.Commands as Commands
-import Ioc.Container
-import qualified Ioc.Container as Commands
-import qualified Ioc.Container as Repositories
-import Ioc.Repositories as Repositories
+import qualified Data.CreateName
+import qualified Data.GetNames
+import qualified Data.UpdateName
+import qualified Domain.GetNames
+import qualified Ioc.Commands as Commands
+import qualified Ioc.Container as Ioc
+import qualified Ioc.Repositories as Repositories
 
-container :: Container
+container :: Ioc.Container
 container =
-  [ Commands.Command (Commands.GetNames GetNamesCommand.getNames),
-    Repositories.Repository (Repositories.CreateName CreateNameRepository.createName),
-    Repositories.Repository (Repositories.UpdateName UpdateNameRepository.updateName),
-    Repositories.Repository (Repositories.GetNames GetNamesRepository.getNames)
+  [ Ioc.Command (Commands.GetNames Domain.GetNames.getNames),
+    Ioc.Repository (Repositories.CreateName Data.CreateName.createName),
+    Ioc.Repository (Repositories.UpdateName Data.UpdateName.updateName),
+    Ioc.Repository (Repositories.GetNames Data.GetNames.getNames)
   ]
 
-doSomething :: ReaderT Container IO ()
-doSomething = do
+innerPerform :: ReaderT Ioc.Container IO ()
+innerPerform = do
   container <- ask
-  let Commands.Command command = head container
-  let Commands.GetNames getNames = command
-  let foo = getNames ""
 
-  liftIO $ putStr $ head foo
-  liftIO $ putStr "Hello"
+  liftIO $ putStrLn "Hey from inside innerPerform..."
+
+perform :: ReaderT Ioc.Container IO ()
+perform = do
+  container <- ask
+  let Ioc.Command command = head container
+  let Commands.GetNames getNames = command
+  let name = head $ getNames "parameter"
+
+  liftIO $ putStrLn name
+  innerPerform
 
 main :: IO ()
 main = do
-  runReaderT doSomething container
+  runReaderT perform container
